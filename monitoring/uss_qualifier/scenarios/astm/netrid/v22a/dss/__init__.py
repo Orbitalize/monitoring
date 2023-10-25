1d0bad14568f082cd7c441c5f9eb985850c414cc
Julien Perrochet <julien.perrochet@orbitalize.com>
2023-10-25 11:57:11 +0200

    [uss-qualifier] DSS0030 port ISA-subscriptions interactions test

--- monitoring/uss_qualifier/scenarios/astm/netrid/v22a/dss/__init__.py
+++ monitoring/uss_qualifier/scenarios/astm/netrid/v22a/dss/__init__.py
@@ -1,4 +1,5 @@
 from .isa_simple import ISASimple
 from .isa_validation import ISAValidation
+from .isa_subscription_interactions import ISASubscriptionInteractions
 from .subscription_validation import SubscriptionValidation
 from .crdb_access import CRDBAccess
